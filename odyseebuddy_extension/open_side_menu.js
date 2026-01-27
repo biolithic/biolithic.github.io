c41e111b6a6ecf3cd1fab6798f2a94f670d2580c
biolithic <pathoses_bicyclism@dralias.com>
2026-01-26 19:17:54 -0800

    updated for 2026

--- odyseebuddy_extension/open_side_menu.js
+++
@@ -1,9 +0,0 @@
-/* Copyright 2023 biolithic. All rights reserved. */
-var x = document.getElementById('scbtSideMenu');
-if (x) {
-	if (x.classList.contains('scbt-bl') ) {
-		x.classList.remove('scbt-bl');
-	} else {
-		x.classList.add('scbt-bl');
-	}
-}
